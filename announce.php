2a255c9043eb208962bf9fc95da543ed93cb9496
ashleshb <ashleshb279@gmail.com>
2022-07-23 12:17:35 +0530

    Announce page title modified

--- announce.php
+++ announce.php
@@ -14,7 +14,7 @@ include 'config.php';
     <meta name="author" content="Template Mo">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
-    <title>List OF Announcements</title>
+    <title>List Of Announcements</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
